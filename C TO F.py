1a3aa2cdc9c1b4346c6fddc82ccc47cfbe9a457f
Zargham Abbas <zarghamabbas859@gmail.com>
2024-03-13 21:08:23 +0500

    Add files via upload

---
+++ C TO F.py
@@ -0,0 +1,17 @@
+# Zargham-abbas
+# zarghamabbas859@gmail.com
+
+# Question No. 6:
+#Write a Python program to convert Celsius to Fahrenheit.
+
+# First we declare our desired variables and use input function
+celsius=int(input("enter a temperature in celsius :"));
+
+# Now we use print function to convert temprature in celcius to fahrenhiet using formula.
+fahrenheit=(celsius*(3/5)+32);
+
+# Print fahrenheit.
+print('fahrenheit=',fahrenheit);
+
+# Run the program.
+# Done!
